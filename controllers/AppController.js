b7d71a11fea4bee1bf0c617a7dd54f44d98a2082
samdaphbynet <lj8@hotmail.fr>
2024-04-02 11:08:02 +0000

    correct importation dbClient

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,11 @@
 import redisClient from "../utils/redis"
-import db from "../utils/db"
+import dbClient from "../utils/db"
 
 class AppController {
     static getStatus(req, res) {
         const status = {
             redis: redisClient.isAlive(),
-            db: db.isAlive()
+            dbClient: db.isAlive()
         }
 
         res.status(200).send(status)
@@ -13,8 +13,8 @@ class AppController {
 
     static async getStats(req, res) {
         const stats = {
-            users: await db.nbUsers(),
-            files: await db.nbFiles()
+            users: await dbClient.nbUsers(),
+            files: await dbClient.nbFiles()
         }
         res.status(200).send(stats)
     }
