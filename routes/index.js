be52da061351d9ff061e43df4ada1127b1952c0c
samdaphbynet <lj8@hotmail.fr>
2024-04-03 12:51:09 +0000

    added new endpoint for users

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 const AppController = require("../controllers/AppController");
+const UsersController = require("../controllers/UsersController");
 
 router.get("/status", AppController.getStatus);
 
 router.get("/stats", AppController.getStats);
+
+router.post("/users", (req, res) => {
+  UsersController.postNew(req, res);
+});
 
 module.exports = router;
