7bfb4b81ef5c1e903b388c863fea90498560cffd
samdaphbynet <lj8@hotmail.fr>
2024-04-08 12:19:31 +0000

    add 3 new endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,25 @@ const express = require("express");
 const router = express.Router();
 const AppController = require("../controllers/AppController");
 const UsersController = require("../controllers/UsersController");
+const {AuthController} = require("../controllers/AuthController");
 
-router.get("/status", AppController.getStatus);
+router.get("/status", (req, res) => {
+  AppController.getStatus(req, res)
+});
 
-router.get("/stats", AppController.getStats);
+router.get("/stats", (req, res) => {
+  AppController.getStats(req, res)
+});
+
+router.get('/connect', (req, res) => {
+  AuthController.getConnect(req, res)
+})
+
+router.get("/disconnect", (req, res) => {
+  AuthController.getDisconnect(req, res)
+})
+
+router.get("/users/me", UsersController.getMe)
 
 router.post("/users", (req, res) => {
   UsersController.postNew(req, res);
