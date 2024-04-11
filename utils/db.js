58543a6a4e056a8046b819c3fadc8a34ecd052ea
samdaphbynet <lj8@hotmail.fr>
2024-04-11 11:24:32 +0000

    handler error  Strings must use singlequote  quotes

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || "files_manager";
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
@@ -10,8 +10,8 @@ class DBClient {
     MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
       if (!error) {
         this.db = client.db(DB_DATABASE);
-        this.users = this.db.collection("users");
-        this.files = this.db.collection("files");
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
       } else {
         console.log(error.message);
         this.db = false;
@@ -24,11 +24,11 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.db.collection("users").countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.db.collection("files").countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 }
 
