be77b450216382b0bff7f35147ab5540ad936225
samdaphbynet <lj8@hotmail.fr>
2024-04-11 11:38:31 +0000

    handler error ESlint

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const routes = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/", routes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log('Server running on port', PORT);
