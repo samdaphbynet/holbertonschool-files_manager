f58ecbda87a6bcf21b9d85f52e7444f577568cd9
samdaphbynet <lj8@hotmail.fr>
2024-04-11 12:07:34 +0000

    fixed error ESlint

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
-const AppController = require("../controllers/AppController");
-const UsersController = require("../controllers/UsersController");
-const {AuthController} = require("../controllers/AuthController");
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const {AuthController} = require('../controllers/AuthController');
 
-router.get("/status", (req, res) => {
+router.get('/status', (req, res) => {
   AppController.getStatus(req, res)
 });
 
-router.get("/stats", (req, res) => {
+router.get('/stats', (req, res) => {
   AppController.getStats(req, res)
 });
 
@@ -17,14 +17,18 @@ router.get('/connect', (req, res) => {
   AuthController.getConnect(req, res)
 })
 
-router.get("/disconnect", (req, res) => {
+router.get('/disconnect', (req, res) => {
   AuthController.getDisconnect(req, res)
 })
 
-router.get("/users/me", UsersController.getMe)
+router.get('/users/me', UsersController.getMe)
 
-router.post("/users", (req, res) => {
+router.post('/users', (req, res) => {
   UsersController.postNew(req, res);
 });
+
+router.post('/files', (req, res) => {
+  FilesController.postUpload(req, res)
+})
 
 module.exports = router;
